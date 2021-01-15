0a65ee5bba344f24c5b036e391ddc98d982d79e6
SYkim <rvnlogin@gmail.com>
2021-01-15 13:16:43 +0900

    Add files via upload

---
+++ 1207_001_Operator_연산자.py
@@ -0,0 +1,13 @@
+print(25 // 10**2 + 1)
+
+a, b = "100", 200
+print(a + str(b))
+
+a = 3
+a += 1
+a *= 2
+a -= 1
+print(a)
+
+year = 2000
+print(((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0))
