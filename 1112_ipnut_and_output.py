b63e7100f62bac0b1268d60d7d969ee845cbf84b
SYkim <rvnlogin@gmail.com>
2021-02-21 18:01:22 +0900

    Delete 1112_ipnut_and_output.py

--- 1112_ipnut_and_output.py
+++
@@ -1,6 +0,0 @@
-name = input("What's your name?")
-print("Welcome,", name, "!")
-age = int(input("How're you old?"))
-# input 함수로 입력받은 데이터는 기본 문자형 데이터로 인식되기 때문에 int라고 따로 데이터 형식을 알려줘야 한다.
-print("You're going to be", age + 10, "years old after 10 years, right?")
-# print 함수 안에서 변수에 바로 숫자를 더하거나 할 수 있다.
